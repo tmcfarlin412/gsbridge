0867e046b30960f7379b47926108bf17993132e8
Tom McFarlin <tmcfarlin412@gmail.com>
2020-04-04 11:24:38 -0500

    ios gamesparks sdk integration

--- ios/Classes/GsbridgePlugin.m
+++ ios/Classes/GsbridgePlugin.m
@@ -1,29 +1,68 @@
 #import "GsbridgePlugin.h"
+#import "GS.h"
+#import "GSAPI.h"
 
 @implementation GsbridgePlugin
+
+static GS *gs;
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"gsbridge"
-            binaryMessenger:[registrar messenger]];
-  GsbridgePlugin* instance = [[GsbridgePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    
+    gs = [[GS alloc] initWithApiKey:@"q391231asyX3" andApiSecret:@"sCLhBW8pr4a1gbRv5t2labw07pJST6Jf" andCredential:@"device" andPreviewMode:true];
+    [gs setAvailabilityListener:^ (BOOL available) {
+        NSLog(@"Gsbridge: GS availablilty listener!");
+    }];
+    
+    [gs setAuthenticatedListener:^(NSString* playerId) {
+        NSLog(@"Gsbridge: GS authenticated listener!");
+    }];
+    
+    GSMessageListener* listener = [[GSMessageListener alloc] init];
+    
+    [listener onGSScriptMessage:^(GSScriptMessage* message) {
+        NSLog(@"%@", message);
+    }];
+    
+    [gs setMessageListener:listener];
+    [gs connect];
+    
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"gsbridge"
+                                     binaryMessenger:[registrar messenger]];
+    GsbridgePlugin* instance = [[GsbridgePlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"authenticate" isEqualToString:call.method]) {
-     NSDictionary<NSString *, id> *_result = @{
+    if ([@"authenticate" isEqualToString:call.method]) {
+        
+        GSAuthenticationRequest* request = [[GSAuthenticationRequest alloc] init];
+        
+        [request setPassword:@"test"];
+        [request setUserName:@"test"];
+        [request setCallback:^ (GSAuthenticationResponse* response) {
+            NSString* authToken = [response getAuthToken];
+            NSString* displayName = [response getDisplayName];
+            //BOOL newPlayer = [response getNewPlayer];
+            //NSDictionary* scriptData = [response getScriptData];
+            GSPlayer* switchSummary = [response getSwitchSummary];
+            NSString* userId = [response getUserId];
+            NSDictionary<NSString *, id> *_result = @{
                 @"status" : @"success",
-                @"authToken" : @"",
-                @"displayName" : @"",
-                @"authToken" : @"",
-                @"newPlayer" : @"",
-                @"switchSummary" : @"",
-                @"userId" : @"",
+                @"authToken" : authToken,
+                @"displayName" : displayName,
+                @"newPlayer" : @"tmp",
+                @"switchSummary" : switchSummary,
+                @"userId" : userId,
             };
-     result(_result);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+            result(_result);
+            
+            
+        }];
+        [gs send:request];
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
